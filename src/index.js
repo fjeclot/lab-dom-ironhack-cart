25a3fb7a1f3453c1eb98b9a1a739a1ea7d1b0f53
fjeclot <jcamposd90@gmail.com>
2024-06-27 04:52:30 +0200

    text

--- src/index.js
+++ src/index.js
@@ -3,21 +3,45 @@
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
 
-  //... your code goes here
+  let priceElement = product.querySelector(".price span");
+  let quantityElement = product.querySelector(".quantity input");
+
+  let price = priceElement.innerText;
+  let quantity = quantityElement.value;
+
+  let subtotalPrice = price * quantity;
+
+  let subtotalElement = product.querySelector(".subtotal span");
+  subtotalElement.innerText = subtotalPrice;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  
+
+  const products = document.querySelectorAll(".product");
+
+  for (let i = 0; i < products.length; i++){
+    updateSubtotal(products[i]);
+  }
 
   // ITERATION 3
-  //... your code goes here
+  
+  let total = 0
+
+  for(let i = 0; i < products.length; i++){
+    let subtotal = Number(products[i].querySelector('.subtotal span').innerText);
+    total += subtotal
+  }
+
+  let totalElement = document.querySelector("#total-value span");
+  totalElement.innerText = total
 }
 
 // ITERATION 4
@@ -26,12 +50,25 @@ function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
   //... your code goes here
+
+  const buttonClicked = target;
+  const productElement = buttonCliked.closest(".product");
+  let totalPrice = document.querySelector("#total-value span");
+  let productSubtotal = productElement.querySelector(".subtotal span");
+
+  newPrice = Number(totalPrice.innerText) - number(productSubtotal.innerText);
+  totalPrice.innerText = newPrice
+  productElement.remove();
+
 }
 
 // ITERATION 5
 
 function createProduct() {
   //... your code goes here
+
+  const newProductInput = document.querySelector(`tfoot tr td input[type="text"]`);
+  const newPriceInput = document.querySelector(`tfoot tr td input[type="number"]`);
 }
 
 window.addEventListener('load', () => {
